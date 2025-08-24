13d383f7e9d3d4352209228cc2ff0a0be1fdd6f9
HankChou <p30205@gmail.com>
2025-08-25 01:49:18 +0800

    feat(login): Modify root route become login page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,42 @@
-<script>
+<script lang="ts">
+  import { supabase } from '$lib/supabase/client';
   import UserIcon from '$lib/icons/UserIcon.svelte';
   import LockIcon from '$lib/icons/LockIcon.svelte';
+  import { goto } from '$app/navigation';
 
+  let email = $state('');
+  let password = $state('');
+
+  async function login(e: SubmitEvent) {
+    e.preventDefault();
+    const { data, error } = await supabase.auth.signInWithPassword({ email, password });
+    console.log('data: ', data);
+    console.log('error: ', error);
+
+    if (!error) {
+      await goto('/dashboard');
+    }
+  }
 </script>
 
 <div class="flex h-screen items-center justify-center">
-  <fieldset class="fieldset w-xs gap-5 rounded-box">
-    <label class="input">
-      <UserIcon className="h-5" />
-      <input type="text" id="account" name="account" placeholder="帳號" />
-    </label>
-
-    <label class="input">
-      <LockIcon className="h-5" />
-      <input type="password" id="password" name="password" placeholder="密碼" />
-    </label>
-
-    <button class="btn btn-outline btn-primary">登入</button>
-  </fieldset>
+  <form onsubmit={login}>
+    <fieldset class="fieldset w-xs gap-5 rounded-box">
+      <label class="input">
+        <UserIcon className="h-5" />
+        <input type="email" id="email" name="email" bind:value={email} placeholder="信箱" />
+      </label>
+      <label class="input">
+        <LockIcon className="h-5" />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          bind:value={password}
+          placeholder="密碼"
+        />
+      </label>
+      <button type="submit" class="btn btn-outline btn-primary">登入</button>
+    </fieldset>
+  </form>
 </div>
