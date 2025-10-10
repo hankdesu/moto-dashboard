e063d3e3daa1b99dec12ec739361453a5e01638a
HankChou <p30205@gmail.com>
2025-10-11 00:11:20 +0800

    feat(ThemeController): Add ThemeController into Sidebar

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
+  import ThemeController from "./ThemeController.svelte";
+
   const menuItems = [{ label: '車籍資料', page: '/dashboard' }];
 </script>
 
-<ul class="menu h-screen w-56 menu-lg rounded-box bg-base-200">
+<ul class="menu h-screen w-56 menu-lg rounded-box bg-base-200 gap-2.5">
+  <li class="flex items-end">
+    <ThemeController />
+  </li>
   {#each menuItems as item}
     <li><a href={item.page}>{item.label}</a></li>
   {/each}
